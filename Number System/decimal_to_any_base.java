f13b6ad538e7680b16e170d894417ba3c5a23704
Nitin Mankar <96859511+Ni-t-in@users.noreply.github.com>
2022-07-18 21:25:33 +0530

    decimal_to_any_base.java

--- Number System/decimal_to_any_base.java
+++ Number System/decimal_to_any_base.java
@@ -1,4 +1,5 @@
 //will convert decimal to binary and octal but not hexadecimal since it contains character too.
+
 public static int decimalToAnyBase(int number, int base){
 	
 	//taking initial resultant 0
