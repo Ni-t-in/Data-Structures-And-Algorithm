146d86a713a0cec0c0d08b5c27cf2adf3c3795ab
Nitin Mankar <96859511+Ni-t-in@users.noreply.github.com>
2022-07-18 21:29:56 +0530

    decimal_to_any_base.java

--- Number System/decimal_to_any_base.java
+++ Number System/decimal_to_any_base.java
@@ -11,15 +11,14 @@ public static int decimalToAnyBase(int number, int base){
 	//execute a loop until number become 0
  	while(number > 0){
 	
-	//it will give digits of resultant
+		//it will give digits of resultant
   	int remainder = number % base;
 		
-	//reducing the number by dividing it with base
-    number = number / base;
+  	//reducing the number by dividing it with base
+  	number = number / base;
 	
-    resultant = resultant + (remainder * multiplier);
-    multiplier *= 10;
-		
+  	resultant = resultant + (remainder * multiplier);
+  	multiplier *= 10;
   }
   return resultant;
 }
