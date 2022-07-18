ff9b8d3dbcc81689e076ef72c82592e8aa638aba
Nitin Mankar <96859511+Ni-t-in@users.noreply.github.com>
2022-07-18 21:33:29 +0530

    any_base_addition.java

--- Number System/any_base_addition.java
+++ Number System/any_base_addition.java
@@ -1,20 +1,27 @@
 //will perform sum for binary and octal base but not for hexadecimal since it contains character too
 public static int anyBaseSum(int base, int number1, int number2){
+  
   int resultant = 0;
   int multiplier = 1;
   int carry = 0;
+  
   while(number1 > 0 || number2 > 0 || carry > 0){
+    
     //extracting single digit from numbers
     int digit1 = number1 % 10;
     int digit2 = number2 % 10;
+    
     //reducing the numbers
     number1 = number1 / 10;
     number2 = number2 / 10;
+    
     //calculating sum of digits       
     int digSum = digit1 + digit2 + carry;
+    
     //converting it into desired base
     carry = digSum / base;
     digSum = digSum % base;
+    
     //forming the resultant       
     resultant += digSum * multiplier;
     multiplier *= 10;
