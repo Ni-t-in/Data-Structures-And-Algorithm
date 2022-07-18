193cfa1ce325ae5649ff4fe2fc411cb36ed0e098
Nitin Mankar <96859511+Ni-t-in@users.noreply.github.com>
2022-07-18 21:19:09 +0530

    any_base_addition.java

--- Number System/any_base_addition.java
+++ Number System/any_base_addition.java
@@ -1,4 +1,4 @@
-//will perform sum for binary and octal base and not for hexadecimal since it contains character too
+//will perform sum for binary and octal base but not for hexadecimal since it contains character too
 public static int anyBaseSum(int base, int number1, int number2){
   int resultant = 0;
   int multiplier = 1;
