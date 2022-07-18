1cae621c430aff28d444254be1d70a058a7ac34f
Nitin Mankar <96859511+Ni-t-in@users.noreply.github.com>
2022-07-18 21:41:50 +0530

    any_base_addition.java

--- Number System/any_base_addition.java
+++ Number System/any_base_addition.java
@@ -3,29 +3,30 @@
 public static int anyBaseSum(int base, int number1, int number2){
     
     int resultant = 0;
-	int multiplier = 1;
-	int carry = 0;
-  
-	while(number1 > 0 || number2 > 0 || carry > 0){
+    int multiplier = 1;
+    int carry = 0;
+
+    while(number1 > 0 || number2 > 0 || carry > 0){
     
-		//extracting single digit from numbers
-		int digit1 = number1 % 10;
-		int digit2 = number2 % 10;
+    //extracting single digit from numbers
+    int digit1 = number1 % 10;
+    int digit2 = number2 % 10;
     
-		//reducing the numbers
-		number1 = number1 / 10;
-		number2 = number2 / 10;
+    //reducing the numbers
+    number1 = number1 / 10;
+    number2 = number2 / 10;
     
-		//calculating sum of digits       
-		int digSum = digit1 + digit2 + carry;
+    //calculating sum of digits       
+    int digSum = digit1 + digit2 + carry;
     
-		//converting it into desired base
-		carry = digSum / base;
-		digSum = digSum % base;
+    //converting it into desired base
+    carry = digSum / base;
+    digSum = digSum % base;
     
-		//forming the resultant       
-		resultant += digSum * multiplier;
-		multiplier *= 10;
-  	}
-  	return resultant;
+    //forming the resultant       
+    resultant += digSum * multiplier;
+    multiplier *= 10;
+	    
+    }
+    return resultant;
 }
